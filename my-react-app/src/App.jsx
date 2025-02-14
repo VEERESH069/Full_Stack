dc7f766519003c304ef340d2b9623e4a43ff12c5
VEERESH069 <116574954+VEERESH069@users.noreply.github.com>
2025-02-14 12:36:49 +0530

    basic profile done

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,20 @@
+import  React  from 'react'
+import './App.css'
+
+const Homepage = ( ) =>{
+
+  return (
+    <>
+      <div  style={{ textAlign: 'center', padding: '20px' }}>
+      <h1 href = 'https://mail.google.com/mail/u/1/#chat/dm/3Yd7NsAAAAE'>Veeresh S k</h1>
+      <p>veereshsk1249@gmail.com</p>
+      <p>12345678910</p>
+      </div>
+
+      
+      
+    </>
+  );
+};
+
+export default Homepage;
